c33e0b1d5d47d84c966e8b0721d7c625f9dd4510
konradkriehmig <konrad.kriehmig@insead.edu>
2026-03-25 19:57:56 +0000

    removed bootstrap logic

--- morning.tf
+++
@@ -1,6 +0,0 @@
-$env:ARM_SUBSCRIPTION_ID="8d9e4373-f610-460e-8c58-e1dc091ba829"
-cd bootstrap
-terraform apply -target=azurerm_role_assignment.tfstate_blob -auto-approve
-terraform apply -auto-approve
-cd ../infra
-terraform init
